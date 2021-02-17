a71dae77dd207bbb4db8f00e0d5e7160c432c7aa
yaranegm22 <66703170+yaranegm22@users.noreply.github.com>
2021-02-17 15:34:25 +0200

    Update user.dart

--- lib/user.dart
+++ lib/user.dart
@@ -11,18 +11,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Open the database and store the reference.
   final Future<Database> database = openDatabase(
-    // Set the path to the database. Note: Using the `join` function from the
-    // `path` package is best practice to ensure the path is correctly
-    // constructed for each platform.
+    
     join(await getDatabasesPath(), 'Userr_database.db'),
-    // When the database is first created, create a table to store dogs.
     onCreate: (db, version) {
       return db.execute(
         "CREATE TABLE users(id INTEGER PRIMARY KEY, fullname TEXT, faculty TEXT ,email TEXT, password TEXT)",
       );
     },
-    // Set the version. This executes the onCreate function and provides a
-    // path to perform database upgrades and downgrades.
+    
     version: 1,
   );
 
@@ -30,9 +26,7 @@ void main() async {
     // Get a reference to the database.
     final Database db = await database;
 
-    // Insert the Dog into the correct table. Also specify the
-    // `conflictAlgorithm`. In this case, if the same dog is inserted
-    // multiple times, it replaces the previous data.
+   
     await db.insert(
       'users',
       user.toMap(),
@@ -44,10 +38,8 @@ void main() async {
     // Get a reference to the database.
     final Database db = await database;
 
-    // Query the table for all The Dogs.
     final List<Map<String, dynamic>> maps = await db.query('users');
 
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
     return List.generate(maps.length, (i) {
       return User(
         id: maps[i]['id'],
@@ -60,30 +52,22 @@ void main() async {
   }
 
   Future<void> updateUser(User user) async {
-    // Get a reference to the database.
     final db = await database;
 
-    // Update the given Dog.
     await db.update(
       'users',
       user.toMap(),
-      // Ensure that the Dog has a matching id.
       where: "id = ?",
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
       whereArgs: [user.id],
     );
   }
 
   Future<void> deleteUser(int id) async {
-    // Get a reference to the database.
     final db = await database;
 
-    // Remove the Dog from the database.
     await db.delete(
       'users',
-      // Use a `where` clause to delete a specific dog.
       where: "id = ?",
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
       whereArgs: [id],
     );
   }
@@ -96,13 +80,10 @@ void main() async {
     password: 'ahmed123',
   );
 
-  // Insert a dog into the database.
   await insertUser(ahmed);
 
-  // Print the list of dogs (only Fido for now).
   print(await users());
 
-  // Update Fido's age and save it to the database.
   ahmed = User(
     id: ahmed.id,
     fullname: ahmed.fullname,
@@ -112,13 +93,10 @@ void main() async {
   );
   await updateUser(ahmed);
 
-  // Print Fido's updated information.
   print(await users());
 
-  // Delete Fido from the database.
   await deleteUser(ahmed.id);
 
-  // Print the list of dogs (empty).
   print(await users());
 }
 
@@ -141,8 +119,7 @@ class User {
     };
   }
 
-  // Implement toString to make it easier to see information about
-  // each dog when using the print statement.
+  /
   @override
   String toString() {
     return 'User{id: $id, fullname: $fullname, faculty: $faculty , email: $email, passowrd: $password }';
